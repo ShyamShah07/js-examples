79f09fa7668112cbcf07bf9150c20725fd1b6809
ShyamShah07 <shyamshah2101@gmail.com>
2026-05-22 08:15:00 +0000

    Variables learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553 
+let accountEmail = "abc@google.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+// accountId = 2
+console.log(accountId)
+if(true) {
+    accountPass = "2"
+    accountEmail = "xyz"
+}
+
+
+
+console.table([accountId , accountEmail , accountPass , accountCity ])
